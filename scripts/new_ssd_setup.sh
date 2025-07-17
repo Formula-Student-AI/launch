f30093db640a39522b092c993d2c8c9ca77e7e1a
sank <sank@sank.com>
2025-07-17 23:29:42 +0100

    Another typo

--- scripts/new_ssd_setup.sh
+++ scripts/new_ssd_setup.sh
@@ -48,7 +48,8 @@ run_stage_1() {
     sudo apt-get install -y software-properties-common curl > /dev/null
     sudo add-apt-repository universe
     sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt-get/sources.list.d/ros2.list > /dev/null
+    sudo mkdir -p /etc/apt/sources.list.d
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
     sudo apt-get update > /dev/null
     sudo apt-get install -y ros-galactic-desktop ros-dev-tools > /dev/null
 
