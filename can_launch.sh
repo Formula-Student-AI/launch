a9b7fa27c4bc9c319d8d974361328e77576f3212
sank <sank@sank.com>
2025-07-17 16:30:23 +0100

    Test different candump cmd

--- can_launch.sh
+++ can_launch.sh
@@ -4,4 +4,4 @@ echo "Setting up can link"
 sudo ip link set up can0 type can bitrate 500000
 
 echo "Starting candump to log CAN traffic"
-stdbuf -o0 candump -l  /home/bristol-fsai/logs/candump_log_$(date +%Y%m%d_%H%M%S).log -ta can0 &
+stdbuf -o0 candump -ta -l /home/bristol-fsai/logs/candump_log_$(date +%Y%m%d_%H%M%S).log can0 &
